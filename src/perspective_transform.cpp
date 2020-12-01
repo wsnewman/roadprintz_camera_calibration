1a2d5d04cb06d0bd480b7a77107ac80ceca5946b
wsnewman <wsn@case.edu>
2020-12-01 13:58:45 -0500

    perspective transform code; closer; still have scaling issue

--- src/perspective_transform.cpp
+++ src/perspective_transform.cpp
@@ -26,8 +26,8 @@ const double RESCALE_FACTOR=2.0; //scale original image down by this factor to f
 
 
 //numbers for Stella's camera:
-const double g_image_width = 2688;
-const double g_image_height = 1520;  
+const int g_image_width = 2688;
+const int g_image_height = 1520;  
 const double   g_image_xc = 1313.144667; //do these change  for rectified image?
 const double   g_image_yc = 774.029343;
 const double g_image_fx = 1637.367343;
@@ -37,11 +37,12 @@ const double g_virt_image_width = 2688; //might want to change these if change r
 const double g_virt_image_height = 1520; 
 
 
+//experiment with values to get desired pixel scaling, pix/meter
+const double VERT_CAM_HEIGHT=3.0; //this actually makes NO difference; but need some value here 
 
-const double VERT_CAM_HEIGHT=3.0; //probably don't need this
+
 const double KPIX = 590.0; //pixels per meter for virtual image; might want to change, e.g., to 1000.0
-//const double CX_CAM = xxx;
-//const double CY_CAM = xxx;
+
 static const std::string OPENCV_WINDOW = "ImageWindow";
 
 Eigen::Affine3d g_affine_virt_cam_wrt_cam;
@@ -50,6 +51,14 @@ Eigen::Affine3d g_affine_virt_cam_wrt_cam;
 cv::Mat g_virtual_image; //(Nu,Nv,CV_8U,cv::Scalar(0));
 cv::Mat g_src;  // need access to this; subsribe?
 sensor_msgs::ImagePtr g_image_msg_ptr;
+
+//precompute how u_virt,v_virt map ont u_cam,v_cam
+//hack: assign black to corner (0,0) in physical image, and
+// for all (u_virt,v_virt) coordinates that do not have a correspondence in physical image,
+// assign u_cam,v_cam = 0,0
+int u_cam_mappings[g_image_width][g_image_height];
+int v_cam_mappings[g_image_width][g_image_height];
+
 
 bool g_got_new_image = false;
 bool g_first_time = true;
@@ -110,7 +119,7 @@ Eigen::Affine3d get_hardcoded_affine_virtual_cam_wrt_sys(void) {
     //           0, 0, -1;   
 
      //   trans<<  2.76796, 0.00720234,    2.88546; //4.5426 rms pixel error in reprojection based on ~1,000 points
-        trans<<  2.77, 0.0,    2.885; //
+        trans<<  2.77, 0.0,    VERT_CAM_HEIGHT; //
     //note: camera origin is about 2.885 meters high, and about 2.768m aft of rear wheels
     //   camera origin is nearly along vehicle centerline, but shifted about 7mm to starboard
 
@@ -137,33 +146,22 @@ bool valid_uv(int u,int v,int Nu,int Nv) {
   return true;
 }
 
-//set camera image as src; set transformed image to be created as dst
-//specify central pixel of input image, (cx_src,cy_src), and foci of input impage, (fx_src,fy_src)
-//specify transform of virtual cam frame w/rt actual cam frame
-//specify height of ideal, virtual image to be populated
-//specify scaling of new iage: kpix = pixels/meter at ideal plane, from height vert_height
-//create new, virtual (dst) image transformed from src image
-bool transform_image(cv::Mat src, cv::Mat dst, Eigen::Affine3d affine_virt_cam_wrt_cam, 
-  double cx_src,double cy_src, double fx_src, double fy_src, double vert_height, double kpix) {
- cv::Vec3b rgbpix; // OpenCV representation of an RGB pixel
+//pre-compute the mappings between (u,v) of virtual camera and (u,v) of physical camera
+void compute_mappings(Eigen::Affine3d affine_virt_cam_wrt_cam, 
+  double cx_src,double cy_src, double fx_src, double fy_src, double vert_height, double kpix)
+{
+    //int u_cam_mappings[g_image_width][g_image_height];
+    //int v_cam_mappings[g_image_width][g_image_height];
+    double cx_virt = g_image_width/2.0;
+    double cy_virt = g_image_height/2.0;
+    Eigen::Vector3d p_wrt_virt,p_wrt_cam;
+  int u_cam,v_cam;
+  p_wrt_virt[2] = vert_height;
+ for (int u_virt=0;u_virt<g_image_width;u_virt++) {
+  for (int v_virt=0;v_virt<g_image_height;v_virt++) {
+      u_cam_mappings[u_virt][v_virt] = 0; //default
+      v_cam_mappings[u_virt][v_virt] = 0;
 
-
- int Nu_virt = dst.cols;
- int Nv_virt = dst.rows;
- int Nu_cam = src.cols;
- int Nv_cam = src.rows;
- double cx_virt = 0.5*Nu_virt;  //virtual image has ideal central pixel
- double cy_virt = 0.5*Nv_virt; 
- Eigen::Vector3d p_wrt_virt,p_wrt_cam;
-  cout<<"transforming image; cx_virt = "<<cx_virt<<"; cy_virt = "<<cy_virt<<", Nu_virt = "<<Nu_virt<<", Nv_virt = "<<Nv_virt<<endl;
-
- //iterate through rows and columns of image to be created (dst)
-
-
- int u_cam,v_cam;
- p_wrt_virt[2] = vert_height;
- for (int u_virt=0;u_virt<dst.cols;u_virt++) {
-  for (int v_virt=0;v_virt<dst.rows;v_virt++) {
     //convert u,v to x,y in virtual frame, as projected onto ideal plane
     p_wrt_virt[0]  = (u_virt-cx_virt)/kpix;
     p_wrt_virt[1]  = (v_virt-cy_virt)/kpix;
@@ -175,15 +173,40 @@ bool transform_image(cv::Mat src, cv::Mat dst, Eigen::Affine3d affine_virt_cam_w
     u_cam = (int) (cx_src + fx_src*p_wrt_cam[0]/p_wrt_cam[2]);
     v_cam = (int) (cy_src + fy_src*p_wrt_cam[1]/p_wrt_cam[2]);
     //cout<<"u_cam, v_cam = "<<u_cam<<", "<<v_cam<<endl;
-    if (valid_uv(u_cam,v_cam,Nu_cam,Nv_cam)) {
-      //cout<<"valid u_cam, v_cam = "<<u_cam<<", "<<v_cam<<endl;
-       //Mat.at<data_Type>(row_num, col_num) = value;
-      dst.at<Vec3b>(v_virt, u_virt) = src.at<Vec3b>(v_cam,u_cam);
-      //rgbpix = src.at<cv::Vec3b>(u_cam, v_cam); //extract an RGB pixel
-      //dst.at<cv::Vec3b>(u_virt, v_virt) = rgbpix;
-    }
+    if (valid_uv(u_cam,v_cam,g_image_width,g_image_height)) { 
+        u_cam_mappings[u_virt][v_virt] = u_cam;
+        v_cam_mappings[u_virt][v_virt] = v_cam;
+    } 
   }
  }
+    
+}
+
+  
+//set camera image as src; set transformed image to be created as dst
+//specify central pixel of input image, (cx_src,cy_src), and foci of input impage, (fx_src,fy_src)
+//specify transform of virtual cam frame w/rt actual cam frame
+//specify height of ideal, virtual image to be populated
+//specify scaling of new iage: kpix = pixels/meter at ideal plane, from height vert_height
+//create new, virtual (dst) image transformed from src image
+bool transform_image(cv::Mat src, cv::Mat dst) {
+ cv::Vec3b rgbpix; // OpenCV representation of an RGB pixel
+
+ //set upper-left pixel to black
+      Vec3b & color = g_virtual_image.at<Vec3b>(0,0);
+        color[0] = 0;
+        color[1] = 0;
+        color[2] = 0;
+        src.at<Vec3b>(0,0)=color;
+
+ int u_cam,v_cam;
+ for (int u_virt=0;u_virt<dst.cols;u_virt++) {
+  for (int v_virt=0;v_virt<dst.rows;v_virt++) {
+      u_cam= u_cam_mappings[u_virt][v_virt];
+      v_cam = v_cam_mappings[u_virt][v_virt];
+      dst.at<Vec3b>(v_virt, u_virt) = src.at<Vec3b>(v_cam,u_cam);
+    }
+  }
 }
 
 
@@ -237,80 +260,23 @@ public:
       return;
     }
 
-    /*
-    if (g_first_time) { //one-shot: is a hack
-        Rect2d r;
-        g_src = cv_ptr->image;
-        r.x = g_x_zoom_left;
-        r.y = g_y_zoom_top;         
-        r.height = ROI_HEIGHT;
-        r.width = ROI_WIDTH;
-        g_r_zoom_window = r;
-        g_imCrop = g_src(r);    // Crop image   
-        resize(g_imCrop, g_imCrop, Size(ROI_WIDTH*ZOOM_FACTOR,ROI_HEIGHT*ZOOM_FACTOR));
-        g_first_time=false;
-    }
-    */
-    // Draw an example circle on the video stream
-    //if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-    //  cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
-    /*
-    int n_vertices = vertices.size();
-    if (n_vertices>1) {
-        for (int i=0;i<n_vertices-1;i++) {
-            if(spray_on_vec[i]) {
-                line(cv_ptr->image,vertices[i],vertices[i+1],cv::Scalar( 255, 0, 0 ),8);
-            }
-        }
-    } */
-    // Update GUI Window
-    //cout<<"calling imshow"<<endl;
-    //Mat src_smaller;
     Mat dst_smaller;
     Mat src = cv_ptr->image;
     g_src = cv_ptr->image;
 
 
-    //Mat imCrop = src;
-
-   /*
-    // Display Cropped Image
-    cv::Scalar blue( 255, 0, 0 );
-    cv::Scalar red( 0, 0, 255 );
-    cv::Scalar green(0,255,0);
-    cv::circle(src_smaller, cv::Point(g_x_ctr, g_y_ctr), 10, green,2);
-    //show endpts and line on whole-view image    
-    cv::circle(src_smaller, cv::Point(g_u_pt1, g_v_pt1), 2, red,2);
-    cv::circle(src_smaller, cv::Point(g_u_pt2, g_v_pt2), 2, blue,2);
-    cv::line(src_smaller,cv::Point(g_u_pt1, g_v_pt1),cv::Point(g_u_pt2, g_v_pt2),green,1,cv::LINE_AA);
-
-
-
-    //double box_x_ctr = g_x_ctr
-    //cv::rectangle( img, cv::Point2f( 10, 10 ), cv::Point2f(100, 100), cv::Scalar( 255, 0, 0 ) );
-    Point2f upper_left((g_x_ctr-BOX_WIDTH/2),(g_y_ctr-BOX_HEIGHT/2));
-    Point2f lower_right((g_x_ctr+BOX_WIDTH/2),(g_y_ctr+BOX_HEIGHT/2));
-    cv::Scalar color( 255, 0, 0 );
-    if (is_first_point) {
-      cv::rectangle(src_smaller,upper_left,lower_right,red, 2);
-    }
-    else {
-        cv::rectangle(src_smaller,upper_left,lower_right,blue, 2);
-    }
- 
-    */
-
-
-/* bool transform_image(cv::Mat src, cv::Mat dst, Eigen::Affine3d affine_virt_cam_wrt_cam, 
-  double cx_src,double cy_src, double fx_src, double fy_src, double vert_height, double kpix)
-*/
-
-
     if (g_first_time) {
       cout<<"first image received; cloning to transformed image"<<endl;
-       g_virtual_image = g_src.clone();
+      g_virtual_image = g_src.clone();
       g_first_time=false;
-
+      if (g_virtual_image.cols != g_image_width) {
+          ROS_ERROR("image is not the expected size; exiting");
+          exit(0);
+      }
+      if (g_virtual_image.rows != g_image_height) {
+          ROS_ERROR("image is not the expected size; exiting");
+          exit(0);
+      }      
      cout<<"size height, width: "<<g_virtual_image.cols<<", "<<g_virtual_image.rows<<endl;
      cout<<"setting all pixels to black"<<endl;
      Vec3b & color = g_virtual_image.at<Vec3b>(0,0);
@@ -334,35 +300,20 @@ public:
     g_image_msg_ptr = cv_bridge::CvImage(std_msgs::Header(), "bgr8", g_virtual_image).toImageMsg();
     //image_pub_.publish(g_virtual_image);
     image_pub_.publish(g_image_msg_ptr);
+
+    ROS_INFO("computing virtual to physical pixel mappings...");
+
+    compute_mappings(g_affine_virt_cam_wrt_cam,g_image_xc,g_image_yc, g_image_fx, g_image_fy, VERT_CAM_HEIGHT,KPIX);
+
+    ROS_INFO("done computing mappings");    
+    
       return;
     }
-
-    transform_image(g_src,g_virtual_image,g_affine_virt_cam_wrt_cam,g_image_xc,g_image_yc, g_image_fx, g_image_fy, VERT_CAM_HEIGHT,KPIX);
+    ROS_INFO("transforming image...");
+    transform_image(g_src,g_virtual_image);
     cout<<"done transforming image"<<endl;
     resize(g_virtual_image, dst_smaller, Size(g_virtual_image.cols/RESCALE_FACTOR,g_virtual_image.rows/RESCALE_FACTOR));
     imshow(OPENCV_WINDOW, dst_smaller);  
-    //imshow(OPENCV_WINDOW, g_src);  
-
-   // Crop image  
-   /* 
-    g_imCrop = g_src(g_r_zoom_window);
-    resize(g_imCrop, g_imCrop, Size(ROI_WIDTH*ZOOM_FACTOR,ROI_HEIGHT*ZOOM_FACTOR));
-    cv::circle(g_imCrop, cv::Point(g_zoom_x_ctr, g_zoom_y_ctr), 20, green,2);
-    if (is_first_point) {
-       cv::rectangle(g_imCrop, cv::Point(0,0), cv::Point(ZOOM_FACTOR*ROI_WIDTH,ZOOM_FACTOR*ROI_HEIGHT), red,2);
-        
-       cv::circle(g_imCrop, cv::Point(g_u_zoomed_pt1, g_v_zoomed_pt1), 2, red,2);
-    }
-    else {
-       cv::rectangle(g_imCrop, cv::Point(0,0), cv::Point(ZOOM_FACTOR*ROI_WIDTH,ZOOM_FACTOR*ROI_HEIGHT), blue,2);
-        
-     cv::circle(g_imCrop, cv::Point(g_u_zoomed_pt2, g_v_zoomed_pt2), 2, blue,2);
-    }
-    
-    imshow(OPENCV_ZOOM_WINDOW,g_imCrop);    */
-    
-    //cv::imshow(OPENCV_WINDOW, cv_ptr->image);
-    //cv::imshow(OPENCV_WINDOW, cv_ptr->image);
 
     cv::waitKey(3);
 
@@ -402,6 +353,8 @@ int main(int argc, char** argv) {
  affine_cam_wrt_sys = get_hardcoded_affine_cam_wrt_sys();
  affine_virt_cam_wrt_sys = get_hardcoded_affine_virtual_cam_wrt_sys();
  g_affine_virt_cam_wrt_cam = affine_cam_wrt_sys.inverse()*affine_virt_cam_wrt_sys;
+ 
+ 
 
  //wait for first image
  ROS_INFO("waiting for camera_image publication...");
